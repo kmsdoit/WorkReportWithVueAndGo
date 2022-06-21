6a3f1415409d491d744a0be88e0815ea7488dc7f
kmsdoit <klmmms882912@naver.com>
2022-06-21 22:14:08 +0900

    회원가입 api 수정 완료

--- backend/model/User.go
+++ backend/model/User.go
@@ -8,7 +8,6 @@ import (
 type User struct {
 	gorm.Model
 	JoinCompanyYear *time.Time `json:"JoinCompanyYear"`
-	Image           string     `gorm:"type:varchar(255)" json:"image" binding:"required"`
 	EmpId           string     `gorm:"type:varchar(255)" json:"EmpId" binding:"required"`
 	Email           string     `gorm:"type:varchar(255)" json:"Email" binding:"required"`
 	Password        string     `gorm:"type:text" json:"password"`
