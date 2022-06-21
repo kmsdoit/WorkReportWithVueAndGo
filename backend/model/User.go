100f2bd2076bf672e2572e635f7bf3ba95298443
kmsdoit <klmmms882912@naver.com>
2022-06-21 14:03:53 +0900

    이미지 업로드 API 제작중

--- backend/model/User.go
+++ backend/model/User.go
@@ -4,6 +4,7 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
+	EmpId    string `gorm:"type:varchar(255)" json:"EmpId" binding:"required"`
 	Email    string `gorm:"type:varchar(255)" json:"Email" binding:"required"`
 	Password string `gorm:"type:text" json:"password"`
 	Name     string `gorm:"type:text" json:"name"`
