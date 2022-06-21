3c262ab4d265d885f1b441c012aa5288ef850262
kmsdoit <klmmms882912@naver.com>
2022-06-21 17:00:36 +0900

    UserModel 수정

--- backend/model/User.go
+++ backend/model/User.go
@@ -1,11 +1,16 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/gorm"
+	"time"
+)
 
 type User struct {
 	gorm.Model
-	EmpId    string `gorm:"type:varchar(255)" json:"EmpId" binding:"required"`
-	Email    string `gorm:"type:varchar(255)" json:"Email" binding:"required"`
-	Password string `gorm:"type:text" json:"password"`
-	Name     string `gorm:"type:text" json:"name"`
+	JoinCompanyYear *time.Time `json:"JoinCompanyYear"`
+	Image           string     `gorm:"type:varchar(255)" json:"image" binding:"required"`
+	EmpId           string     `gorm:"type:varchar(255)" json:"EmpId" binding:"required"`
+	Email           string     `gorm:"type:varchar(255)" json:"Email" binding:"required"`
+	Password        string     `gorm:"type:text" json:"password"`
+	Name            string     `gorm:"type:text" json:"name"`
 }
